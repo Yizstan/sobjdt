120a1ac0a1a45dc0f1ea6ef081907f068c6e3b99
Yizstan <rxo.v.gundam@gmail.com>
2025-06-12 13:26:28 +0700

    add how to commit muanully file

---
+++ sobjdt/Untitled.swift
@@ -0,0 +1,12 @@
+
+//          commit and push manually
+
+//          # 1. Stage your code changes
+//          git add .
+//
+//          # 2. Commit
+//          git commit -m "Describe your update"
+//
+//          # 3. Push to GitHub
+//          git push
+
